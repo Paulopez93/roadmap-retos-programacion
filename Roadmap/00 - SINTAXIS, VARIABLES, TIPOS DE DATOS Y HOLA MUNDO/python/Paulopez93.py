51c8a4e6dd0cca91649d13d46d222c9b8c82d041
Paulopez93 <pz9395programer@gmail.com>
2024-08-08 16:50:16 -0500

    Paulopez93.py
    Reto 00

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/Paulopez93.py
@@ -0,0 +1,38 @@
+## Primer Reto - ##
+## El lenguaje escogido es Python - URl https://www.python.org/
+
+
+# Para crear comentarios en python usamos el simbolo #
+
+"""
+Usando tres dobles comillas al iniciar y cerrando con tres dobles comillas, 
+podemos generar una caja completa para realizar 
+los comentarios que 
+necesitemos.
+
+"""
+
+
+## Creando una Variable
+primera_variable = 50
+
+## no se pueden crear constantes en python pero se puede  dejar el comentario por convencion que es una constante.abs
+
+
+# tipos de datos
+
+numerico = 10
+
+flotante = 0.50
+
+cadena_de_texto = "variable"
+
+booleano = True
+
+
+print ("Hola, primer reto")
+
+print(type(numerico))
+print(type(flotante))
+print(type(cadena_de_texto))
+print(type(booleano))
